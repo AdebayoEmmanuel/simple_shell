1feac9bdf5bca173c718336a6983624958d9d40e
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-05-17 22:50:51 +0000

    ADD support for non interactive mode

--- main.c
+++ main.c
@@ -4,27 +4,40 @@
 #include "main.h"
 
 
-int main(void)
+int main(int ac, char **av)
 {
 	char *cmd_str = NULL;
 	char **args = NULL;
 	
-	/*we want to store the command and its args in an array like {"/bin'ls", "-l", "-a"}*/
-
-
-
+	/*infinite loop to keep collecting command*/
+	while (1)
+	{
+		/*non interactive mode*/
+		if (ac > 1)
+		{
+			/*point arg to second command line argument that ran the program*/
+			args = av + 1;
+		}
+		else
+		{
+	/*interactive mode*/
+	/*read command from user*/
 	/*... enter the command*/
 	cmd_str = enter_command("Command me master: ");
-
 	/*our method to create an array of strings from command and flags*/
 	args = to_args(cmd_str, 10);
-
+		}
 	/*execute command*/
-	if (execve(/*command*/args[0],/*arguments*/args,/*env vairables*/NULL) < 0);
-	{
-		/*an error occured??*/
-		dprintf(STDERR_FILENO, "%s: Command not found\n", args[0]);
-	}
+	execve(/*command*/args[0],/*arguments*/args,/*env vairables*/NULL);
 	
+	 /*an error occured??*/
+	dprintf(STDERR_FILENO, "%s: Command not found\n", args[0]);
+	
+	if (ac > 1)
+		break;
+
+	}
+
+
 	return (0);
 }
